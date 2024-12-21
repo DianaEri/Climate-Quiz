be8cd9deb9a56ccc3d13fd0611b33e09ba06cc60
DianaEri <diana_eriksson@yahoo.com>
2024-12-21 10:57:56 +0000

    Styling changes to home.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import recycleIcon from '../assets/recycle.svg'; 
+import recycleIcon from '../assets/logo_climate.svg';
+import kWhiteIcon from '../assets/k_white.svg'; 
 
 const Logo = () => {
   return (
     <div className="logo-container">
-      <h1 className="logo-klimatet">Klimat</h1>
+      <h1 className="logo-klimatet">
+        <img src={kWhiteIcon} alt="K" className="logo-k-icon" />limat
+      </h1>
       <div className="logo-quizet">
         <span className="logo-quiz">Quizet</span>
         <img src={recycleIcon} alt="Recycle Icon" className="logo-icon" />
