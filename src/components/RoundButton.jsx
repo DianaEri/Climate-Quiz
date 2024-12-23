fd5fb08d11bee5fc75d61c65e930e6793a338515
DianaEri <diana_eriksson@yahoo.com>
2024-12-23 14:46:29 +0000

    Created a striped table for teacher page

--- src/components/RoundButton.jsx
+++ src/components/RoundButton.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUp } from '@fortawesome/free-solid-svg-icons';
+import { faCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 const RoundButton = () => {
   return (
-    <div className="round-button">
-      <FontAwesomeIcon icon={faCircleUp} className="circle-up-icon" />
+    <div className="round-button" style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+      <span className="button-label" style={{ fontSize: '16px', fontWeight: 'bold' }}>
+        Öppna
+      </span>
+      <FontAwesomeIcon icon={faCircleRight} className="circle-up-icon" />
     </div>
   );
 };
 
 export default RoundButton;
+
