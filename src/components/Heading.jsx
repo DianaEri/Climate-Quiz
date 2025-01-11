801b1c7b8b3a4b550db9bae10e6a264d3e844fbc
DianaEri <diana_eriksson@yahoo.com>
2025-01-11 15:31:45 +0000

    Added comments to Heading.jsx

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'; // Importerar React-biblioteket för att använda JSX
+import PropTypes from 'prop-types'; // Importerar PropTypes för att definiera och validera komponentens props
 
+// Heading-komponenten används för att skapa en rubrik med texten som skickas in som prop
+// Den tar emot en prop:
+// - text: Texten som ska visas i rubriken
 const Heading = ({ text }) => {
-  return <h1 className="heading">{text}</h1>;
+  return <h1 className="heading">{text}</h1>; // Returnerar en h1-tag med den angivna texten
 };
 
+// Definierar prop-typer för Heading-komponenten
+// Text-propen är obligatorisk och ska vara en sträng
 Heading.propTypes = {
-  text: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired, // Text måste vara en sträng och den är obligatorisk
 };
 
-export default Heading;
+export default Heading; // Exporterar Heading-komponenten så att den kan användas i andra delar av appen
