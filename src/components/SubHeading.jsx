e2b2ff58f87c67362578a89aa49f6609d49e2d39
DianaEri <diana_eriksson@yahoo.com>
2025-01-11 16:40:22 +0000

    Added comments to SubHeading.jsx

--- src/components/SubHeading.jsx
+++ src/components/SubHeading.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SubHeading = ({ text, className = '' }) => {
-  return <h2 className={`${className} subheading`}>{text}</h2>;  // Dynamically apply the className
+  return <h2 className={`${className} subheading`}>{text}</h2>;  // Dynamiskt tillämpa className
 };
 
 SubHeading.propTypes = {
-  text: PropTypes.string.isRequired,
-  className: PropTypes.string, // Make className optional
+  text: PropTypes.string.isRequired, // Texten för underrubriken
+  className: PropTypes.string, // Gör className valfri
 };
 
-export default SubHeading;
-
-
+export default SubHeading; // Exporterar SubHeading-komponenten
