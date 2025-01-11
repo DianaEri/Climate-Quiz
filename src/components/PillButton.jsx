966ee0514124cc5e517e6fa2dd1db91d0b3ff0ef
DianaEri <diana_eriksson@yahoo.com>
2025-01-11 15:58:08 +0000

    Added comments to PillButton.jsx

--- src/components/PillButton.jsx
+++ src/components/PillButton.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react'; // Importerar React för att använda JSX
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Importerar FontAwesomeIcon för att använda ikoner
 
+// PillButton-komponenten som renderar en knapp med text och en ikon
 const PillButton = ({ onClick, text, icon }) => {
   return (
-    <button className="login-button" onClick={onClick}>
-      <span className="button-text">{text}</span>
-      <FontAwesomeIcon icon={icon} className="button-icon" />
+    <button className="login-button" onClick={onClick}> {/* Knapp som anropar onClick-funktionen när den klickas */}
+      <span className="button-text">{text}</span> {/* Visar texten som skickas in via props */}
+      <FontAwesomeIcon icon={icon} className="button-icon" /> {/* Visar ikonen som skickas in via props */}
     </button>
   );
 };
 
-export default PillButton;
+export default PillButton; // Exporterar PillButton-komponenten för användning i andra delar av appen
+
