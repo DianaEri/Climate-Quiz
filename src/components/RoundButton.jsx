31002c2ee177eca9532fa63d3cb0601fe0205f1b
DianaEri <diana_eriksson@yahoo.com>
2025-01-11 16:31:21 +0000

    Added comments to RoundButton.jsx

--- src/components/RoundButton.jsx
+++ src/components/RoundButton.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { faCircleRight } from '@fortawesome/free-solid-svg-icons'; // Importerar ikonen för höger cirkel
 
 const RoundButton = () => {
   return (
     <div className="round-button" style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+      {/* Texten på knappen */}
       <span className="button-label" style={{ fontSize: '16px', fontWeight: 'bold' }}>
         Öppna
       </span>
+      {/* Ikonen för knappen */}
       <FontAwesomeIcon icon={faCircleRight} className="circle-up-icon" />
     </div>
   );
 };
 
-export default RoundButton;
+export default RoundButton; // Exporterar RoundButton-komponenten
+
 
